bf11f64198b019679966a202b87dd2afb739f822
Akeem ogunfowora <69085674+MasterAKT@users.noreply.github.com>
2021-11-30 21:49:47 -0600

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "this testing look easy for script on Jenkins - version 2"
+echo "this testing look easy for script on Jenkins - version 1"
 echo " this is clear to me now"
 echo "understanding the configuration how to pull repo automation"
